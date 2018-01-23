5eb5c2cca6f95a2bba830225b5e186a086084521
kanika chugh <kanika@kanikas-MacBook-Air.local>
2018-01-23 15:49:08 +0530

    script to perform different operations of time series in given time range with cron

--- auto_schedule.py
+++ auto_schedule.py
@@ -1,15 +1,15 @@
-#from itertools import zip_longest
-from itertools import izip_longest as zip_longest
+from itertools import zip_longest
 import xlrd
 import requests
 import os
+import pandas as pd
 
 def automateType1(url):
     r = requests.get(url)
     with open("bcb_input_10.xlsx", "wb") as code:
         code.write(r.content)
     rb2 = xlrd.open_workbook('bcb_input_1.xlsx') ##### old xls file in current path
-    rb1 = xlrd.open_workbook('bcb_input_10.xlsx') #### new file dowloaded from the url
+    rb1 = xlrd.open_workbook('bcb_input_20.xlsx') #### new file dowloaded from the url
     dumy_out=[]
     sheet1 = rb1.sheet_by_index(0)
     sheet2 = rb2.sheet_by_index(0)
@@ -58,16 +58,49 @@ def automateType2(url):
                 print("Row {} missing".format(rownum+1))
                 
         os.remove('bcb_input_2.xlsx')
-        os.rename('bcb_input_20.xlsx','bcb_input_2.xlsx')
+        os.rename('bcb_input_20.xlsx','bcb_input_2 .xlsx')
             
         return dumy_out_2
             
    
-if __name__=="__main__":
-        try:     
-            output1=automateType1('http://www.bcb.gov.br/pec/Indeco/Ingl/ie5-24i.xlsx')
-            output2=automateType2('http://www.bcb.gov.br/pec/Indeco/Ingl/ie5-26i.xlsx')
-        except:
-            print("file execution done")
+if __name__=="__main__":       
+        output1=automateType1('http://www.bcb.gov.br/pec/Indeco/Ingl/ie5-24i.xlsx')
+        output2=automateType2('http://www.bcb.gov.br/pec/Indeco/Ingl/ie5-26i.xlsx')
+        file1=pd.read_csv('bcb_output_1.csv')
+        file1.loc[len(file1)]=['1/'+str(int(output1[0]))+'/2018',output1[1],output1[2],output1[3],output1[4],output1[5],output1[6],output1[7],output1[8],output1[9],output1[10]] ### final output save with date
+        os.remove('bcb_output_1.csv')
+        file1.to_csv('bcb_output_1.csv')
+        ###### For 2nd output arrangement
+        file2=pd.read_csv("bcb_output_2.csv")
+        
+        if output2[0] == 'Jan':
+            file2.loc[len(file2)] = ['01/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Feb':
+            file2.loc[len(file2)] = ['02/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Mar':
+            file2.loc[len(file2)] = ['03/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Apr':
+            file2.loc[len(file2)] = ['04/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'May':
+            file2.loc[len(file2)] = ['05/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Jun':
+            file2.loc[len(file2)] = ['06/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Jul':
+            file2.loc[len(file2)] = ['07/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Aug':
+            file2.loc[len(file2)] = ['08/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Sep':
+            file2.loc[len(file2)] = ['09/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Oct':
+            file2.loc[len(file2)] = ['10/'+'01'+'/2018',output2[1]]
+        elif output2[0] == 'Nov':
+            file2.loc[len(file2)] = ['11/'+'01'+'/2018',output2[1]]
+        else:
+            file2.loc[len(file2)] = ['12/'+'01'+'/2018',output2[1]]
+        
+        os.remove('bcb_output_2.csv')
+        file2.to_csv('bcb_output_2.csv')
+            
+        
 
 
